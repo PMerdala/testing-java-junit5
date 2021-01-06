76cfbbad37816d0808f5fb71145a2f274f504539
pmadamed <pawel.merdala@adamed.com.pl>
2021-01-06 17:50:49 +0100

    add test for processFindForm

--- src/test/java/pl/pmerdala/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/pl/pmerdala/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -7,11 +7,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.pmerdala.springframework.sfgpetclinic.fauxspring.BindingResult;
+import pl.pmerdala.springframework.sfgpetclinic.fauxspring.Model;
 import pl.pmerdala.springframework.sfgpetclinic.model.Owner;
 import pl.pmerdala.springframework.sfgpetclinic.services.OwnerService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -20,6 +24,8 @@ class OwnerControllerTest {
 
     private static final String REDIRECT_OWNERS_5 = "redirect:/owners/5";
     private static final String OWNERS_CREATE_OR_UPDATE_OWNER_FORM = "owners/createOrUpdateOwnerForm";
+    public static final String OWNERS_FIND_OWNERS = "owners/findOwners";
+    public static final String OWNERS_OWNERS_LIST = "owners/ownersList";
     @Mock
     OwnerService service;
 
@@ -29,9 +35,13 @@ class OwnerControllerTest {
     @Mock
     BindingResult bindingResult;
 
+    @Mock
+    Model model;
+
     @AfterEach
     void tearDown() {
         then(service).shouldHaveNoMoreInteractions();
+        then(model).shouldHaveNoMoreInteractions();
     }
 
     @Test
@@ -101,5 +111,64 @@ class OwnerControllerTest {
         final String viewName = controller.processCreationForm(owner, bindingResult);
         //then
         then(service).should().save(any());
+    }
+
+    @Test
+    void processFindFormEmptyLastNameShouldInvokeService() {
+        //given
+        Owner owner =new Owner(null,null,null);
+        //when
+        String viewName = controller.processFindForm(owner,bindingResult,model);
+        //then
+        then(service).should().findAllByLastNameLike(argThat(arg->arg.equals("%%")));
+    }
+    @Test
+    void processFindFormLastNameTestShouldInvokeService() {
+        //given
+        Owner owner =new Owner(null,null,"Test");
+        //when
+        String viewName = controller.processFindForm(owner,bindingResult,model);
+        //then
+        then(service).should().findAllByLastNameLike(argThat(arg->arg.equals("%Test%")));
+    }
+
+    @Test
+    void processFindFormNotOwnerFoundShouldGoToFindsView() {
+        //given
+        Owner owner =new Owner(null,null,"Test");
+        //when
+        String viewName = controller.processFindForm(owner,bindingResult,model);
+        //then
+        then(service).should().findAllByLastNameLike(any());
+        assertThat(viewName).isEqualTo(OWNERS_FIND_OWNERS);
+    }
+    @Test
+    void processFindFormOneOwnerFoundShouldRedirectToOwnerPage() {
+        //given
+        Owner owner =new Owner(null,null,"Test");
+        List<Owner> findOwners = new ArrayList<>();
+        findOwners.add(new Owner(5L,"Test","Test"));
+        given(service.findAllByLastNameLike(any())).willReturn(findOwners);
+        //when
+        String viewName = controller.processFindForm(owner,bindingResult,model);
+        //then
+        then(service).should().findAllByLastNameLike(any());
+        assertThat(viewName).isEqualTo(REDIRECT_OWNERS_5);
+        then(model).shouldHaveNoInteractions();
+    }
+    @Test
+    void processFindFormTwoOwnerFoundShouldGoOwnerList() {
+        //given
+        Owner owner =new Owner(null,null,"Test");
+        List<Owner> findOwners = new ArrayList<>();
+        findOwners.add(new Owner(5L,"Test","Test"));
+        findOwners.add(new Owner(1L,"Pre","PretestSub"));
+        given(service.findAllByLastNameLike(any())).willReturn(findOwners);
+        //when
+        String viewName = controller.processFindForm(owner,bindingResult,model);
+        //then
+        then(service).should().findAllByLastNameLike(any());
+        assertThat(viewName).isEqualTo(OWNERS_OWNERS_LIST);
+        then(model).should().addAttribute(eq("selections"),any(List.class));
     }
 }
